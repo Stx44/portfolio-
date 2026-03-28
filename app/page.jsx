24e72e6c09d5ba5732133448d800d91869805033
joao <jvcs0091@gmail.com>
2026-03-28 11:01:13 -0300

    tela alterada

--- app/page.jsx
+++ app/page.jsx
@@ -1,42 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-<<<<<<< HEAD
-
-const ProjectCard = ({ category, title, description, imgSrc, link, bg }) => (
-  <section className={`w-full flex items-center justify-center py-10 ${bg}`}>
-    <motion.div whileHover={{scale: 1.05}} whileTap={{scale: 0.95}} className="max-w-screen-xl w-full px-10">
-      <div className="mx-auto max-w-5xl bg-slate-800/50 p-12 rounded-3xl border border-slate-700 shadow-2xl">
-        <div className="flex items-center gap-8">
-          <img 
-            className="hidden md:block w-1/3 mask-radial-[100%_100%] mask-radial-from-75% mask-radial-at-left object-contain" 
-            src={imgSrc} 
-            alt={title} 
-          />
-          <div className="font-medium">
-            <p className="font-mono text-xs text-blue-500 uppercase tracking-widest">{category}</p>
-            <h2 className="mt-2 text-2xl font-bold text-white">{title}</h2>
-            <p className="mt-2 text-base leading-relaxed text-gray-300 text-balance">
-              {description}
-            </p>
-            <a href={link} target="_blank" rel="noopener noreferrer">
-              <motion.button
-                whileHover={{ scale: 1.05, backgroundColor: "#1d4ed8" }}
-                whileTap={{ scale: 0.95 }}
-                className="mt-5 px-8 py-2 bg-blue-500 text-white font-bold rounded-2xl shadow-lg"
-              >
-                Visitar
-              </motion.button>
-            </a>
-          </div>
-        </div>
-      </div>
-    </motion.div>
-  </section>
-);
-=======
 import Link from 'next/link';
->>>>>>> 4a78545 (tela principal concluida)
 
 export default function Home() {
   const textoOriginal = 'Console.log("Hello world")';
@@ -121,18 +86,7 @@ export default function Home() {
               Estudante do 1º semestre de Ciência da Computação, com foco no desenvolvimento de soluções modernas e aprendizado contínuo. Possuo conhecimentos básicos em JavaScript, com experiência prática na criação de interfaces dinâmicas utilizando React e React Native, além do desenvolvimento de APIs com Node.js. Complemento minha base técnica com a manipulação de bancos de dados relacionais em PostgreSQL.
               <br /><br />
               Busco minha primeira oportunidade para aplicar esses fundamentos em projetos reais e evoluir na trilha de engenharia de software.
-<<<<<<< HEAD
-            </p>
-            <motion.button id="contato"
-              whileHover={{ scale: 1.05, backgroundColor: "#1d4ed8" }}
-              whileTap={{ scale: 0.95 }}
-              className="mt-5 px-8 py-2 bg-blue-500 text-white font-bold rounded-2xl shadow-lg"
-            >
-              Contato
-            </motion.button>
-=======
             </p>  
->>>>>>> 4a78545 (tela principal concluida)
           </div>
           <div className="flex justify-center items-center order-1 md:order-2">
             <img 
@@ -143,47 +97,6 @@ export default function Home() {
           </div>
         </div>
       </section>
-<<<<<<< HEAD
-      
-      <h1 className="text-6xl font-bold text-center py-10 bg-amber-300">
-        Projetos
-      </h1>      
-
-      <ProjectCard 
-        category="Saúde"
-        title="PlusHealth"
-        imgSrc="logo2.svg"
-        link="https://github.com/Stx44/TCC_react_native"
-        bg="bg-slate-900"
-        description="Aplicativo móvel focado na promoção de hábitos saudáveis, desenvolvido como Trabalho de Conclusão de Curso (TCC) em Desenvolvimento de Sistemas. A solução permite que usuários gerenciem metas semanais, monitorem o progresso por meio de indicadores visuais e registrem atividades de alimentação e exercícios. Construído com React Native e Expo."
-      />
-
-      <ProjectCard 
-        category="Social"
-        title="VeriFact"
-        imgSrc="verifact.svg"
-        link="https://veri-fact-eight.vercel.app/"
-        bg="bg-slate-900"
-        description="Aplicativo focado na verificação de informações e combate a notícias falsas. Desenvolvido com Next.js e Tailwind CSS, o projeto utiliza uma interface moderna e responsiva para garantir que o usuário tenha acesso a dados confiáveis de forma rápida e eficiente."
-      />
-
-      <footer className="bg-amber-50" href="contato">
-        <div className="flex flex-col grid grid-cols-3 items-center justify-center text-center">
-          <div>
-            <h1>LinkdIn</h1>
-          </div>
-
-          <div>
-            <h1>Instagram</h1>
-          </div>
-
-          <div>
-            <h1>GitHub</h1>
-          </div>
-        </div>
-      </footer>
-=======
->>>>>>> 4a78545 (tela principal concluida)
     </main>
   );
 }
